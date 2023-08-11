85e5868724155d67075545c07041088304e214d0
Nick Olson <naolson.1819@gmail.com>
2023-08-11 16:32:59 -0500

    added seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias ProgrammingPhoenixLiveview.Catalog
+
+products = [
+  %{
+    name: "Programming Phoenix 1.4",
+    description: "The best book ever!",
+    sku: 5316_3545,
+    unit_price: 39.95
+  },
+  %{
+    name: "Programming Elixir 1.6",
+    description: "The second best book ever!",
+    sku: 6316_3546,
+    unit_price: 49.95
+  },
+  %{
+    name: "Programming Ecto",
+    description: "The third best book ever!",
+    sku: 7316_3547,
+    unit_price: 59.95
+  }
+]
+
+Enum.each(products, fn product ->
+  Catalog.create_product(product)
+end)
