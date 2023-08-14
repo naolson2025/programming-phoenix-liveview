c60348c3fe38015c13af2069a12992bffe9ca605
Nick Olson <naolson.1819@gmail.com>
2023-08-14 14:53:46 -0500

    added survey context & schema

---
+++ lib/programming_phoenix_liveview/survey/rating.ex
@@ -0,0 +1,24 @@
+defmodule ProgrammingPhoenixLiveview.Survey.Rating do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias ProgrammingPhoenixLiveview.Catalog.Product
+  alias ProgrammingPhoenixLiveview.Accounts.User
+
+  schema "ratings" do
+    field :stars, :integer
+    belongs_to :user, User
+    belongs_to :product, Product
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(rating, attrs) do
+    rating
+    |> cast(attrs, [:stars, :user_id, :product_id])
+    |> validate_required([:stars, :user_id, :product_id])
+    |> validate_inclusion(:stars, 1..5)
+    |> unique_constraint(:product_id, name: :index_ratings_on_product_id)
+  end
+end
