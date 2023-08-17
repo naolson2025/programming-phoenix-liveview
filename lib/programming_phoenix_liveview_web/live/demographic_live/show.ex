b20c925a58dc1d6a0a63b4b1bba8194b51dc9baf
Nick Olson <naolson.1819@gmail.com>
2023-08-17 07:43:19 -0500

    working survey

--- lib/programming_phoenix_liveview_web/live/demographic_live/show.ex
+++ lib/programming_phoenix_liveview_web/live/demographic_live/show.ex
@@ -2,6 +2,7 @@ defmodule ProgrammingPhoenixLiveviewWeb.DemographicLive.Show do
   use Phoenix.Component
   use Phoenix.HTML
   alias ProgrammingPhoenixLiveview.Survey.Demographic
+  alias ProgrammingPhoenixLiveviewWeb.CoreComponents
 
   # &#x2713; this is unicode that will be rendered as a checkmark
   attr :demographic, Demographic, required: true
@@ -11,10 +12,14 @@ defmodule ProgrammingPhoenixLiveviewWeb.DemographicLive.Show do
       <h2 class="font-medium text-2xl">
         Demographics <%= raw "&#x2713;" %>
       </h2>
-      <ul>
-        <li>Gender: <%= @demographic.gender %></li>
-        <li>Year of birth: <%= @demographic.year_of_birth %></li>
-      </ul>
+      <CoreComponents.table id="demographics" rows={[@demographic]}>
+        <:col label="Gender">
+          <%= @demographic.gender %>
+        </:col>
+        <:col label="Year of birth">
+          <%= @demographic.year_of_birth %>
+        </:col>
+      </CoreComponents.table>
     </div>
     """
   end
